80701cc57ed6c2af260f5af8f632e577a60142a8
Harshith Venkatesh <harshith.venkatesh@freshworks.com>
2024-10-29 12:52:15 +0530

    test: fw-datepicker test case changes added

--- packages/crayons-core/src/components/datepicker/datepicker.e2e.ts
+++ packages/crayons-core/src/components/datepicker/datepicker.e2e.ts
@@ -836,14 +836,14 @@ describe('fw-datepicker', () => {
   it('should update the value of the date input when value is set, when the locale is is', async () => {
     const page = await newE2EPage();
     await page.setContent(
-      '<fw-datepicker locale="is" value="2022-04-20"></fw-datepicker>'
+      '<fw-datepicker locale="is" value="2022--20"></fw-datepicker>'
     );
     await page.waitForChanges();
     const dp = await page.find('fw-datepicker');
     await dp.setProperty('value', '2022-11-20');
     await page.waitForChanges();
     const val = await dp.getProperty('value');
-    expect(val).toBe('20.04.2022');
+    expect(val).toBe('20.11.2022');
     const shadow = await page.find(
       'fw-datepicker >>> fw-input >>> :first-child'
     );
