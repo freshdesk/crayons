e51919cf6764b60cf44130c1f3469965b8d33750
Harshith Venkatesh <harshith.venkatesh@freshworks.com>
2024-11-13 12:44:07 +0530

    fix: remove condition changes for datepicker

--- packages/crayons-core/src/components/datepicker/datepicker.tsx
+++ packages/crayons-core/src/components/datepicker/datepicker.tsx
@@ -1024,11 +1024,10 @@ export class Datepicker {
     if (
       year < this.minYear ||
       year > this.maxYear ||
-      (this.langModule?.code !== 'is' && !isValid(parsedDate)) ||
-      (this.langModule?.code !== 'is' &&
-        !isMatch(val, this.displayFormat, {
-          locale: this.langModule,
-        })) ||
+      !isValid(parsedDate) ||
+      !isMatch(val, this.displayFormat, {
+        locale: this.langModule,
+      }) ||
       !this.isDateWithinMinMaxDate(parsedDate.valueOf(), false)
     ) {
       this.isDateInvalid = !!val;
